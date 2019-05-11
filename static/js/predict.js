e8502a85d9954d1fb38e336334f98232a84ccfe5
alyzaw <alyzaw@gmail.com>
2019-05-11 15:45:22 -0400

    interpret ML output

--- static/js/predict.js
+++ static/js/predict.js
@@ -19,12 +19,12 @@
 
 function makePredictions() {
   // Grab a reference to the dropdown select element
-  //   var input1 = None;
-  //   // var input1 = d3.select("#selDataset1").node().value;
-  //   // var input1 = d3.select("#selDataset1").value;
-  //   var input2 = None;
-  //   var input3 = None;
-  //   var input4 = None;
+//     var input1 = "";
+//   //   // var input1 = d3.select("#selDataset1").node().value;
+//   //   // var input1 = d3.select("#selDataset1").value;
+//     var input2 = "";
+//     var input3 = "";
+//     var input4 = "";
 
   var input1 = d3.select("#selDataset1").node().value;
   // var input1 = d3.select("#selDataset1").node().value;
@@ -33,15 +33,37 @@ function makePredictions() {
   var input3 = d3.select("#selDataset3").node().value;
   var input4 = d3.select("#selDataset4").node().value;
 
-  console.log(input3);
-  console.log(input4);
+//  console.log(input3);
+//  console.log(input4);
 
   // Use the list of sample names to populate the select options
   //   d3.json(`/mlm/(${input1}, ${input2}, ${input3}, ${input4})`).then(() => {
   // var output = document.getElementsByClassName("body");
   d3.json(`/mlm/(${input1}, ${input2}, ${input3}, ${input4})`).then(data => {
-    console.log(data.data)
-    //   });
+    //console.log(data.data);
+    
+    result = data.data;
+
+    switch(result)
+    {
+        case '0':
+        document.getElementById("result-img").src="https://previews.123rf.com/images/vadimdesign/vadimdesign1503/vadimdesign150300021/38032099-contraception-method-surgical-sterilization-icon-family-and-parenthood-pregnancy.jpg";
+        document.getElementById("result-text").innerText="Snip it";
+        break;
+
+        case '1':
+        document.getElementById("result-img").src="../static/css/pill_glyph.PNG";
+        document.getElementById("result-text").innerText="pill";
+        break;
+
+        case '2':
+        document.getElementById("result-img").src="../static/css/iud_glyph.PNG";
+        document.getElementById("result-text").innerText="IUD";
+        break;
+    }
+
+
+      //   });
 
     //   // Use the first sample from the list to build the initial plots
     //   const firstMethod = Method[1];
