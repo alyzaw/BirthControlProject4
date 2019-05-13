0a95dbae43d5bd7bbfb7f9062e1a22d9f8e63069
alyzaw <alyzaw@gmail.com>
2019-05-13 18:47:26 -0400

    cleaned news, add ML dropdowns

--- static/js/predict.js
+++ static/js/predict.js
@@ -19,28 +19,14 @@
 
 function makePredictions() {
   // Grab a reference to the dropdown select element
-//     var input1 = "";
-//   //   // var input1 = d3.select("#selDataset1").node().value;
-//   //   // var input1 = d3.select("#selDataset1").value;
-//     var input2 = "";
-//     var input3 = "";
-//     var input4 = "";
 
   var input1 = d3.select("#selDataset1").node().value;
-  // var input1 = d3.select("#selDataset1").node().value;
-  // var input1 = d3.select("#selDataset1").value;
+
   var input2 = d3.select("#selDataset2").node().value;
   var input3 = d3.select("#selDataset3").node().value;
   var input4 = d3.select("#selDataset4").node().value;
 
-//  console.log(input3);
-//  console.log(input4);
-
-  // Use the list of sample names to populate the select options
-  //   d3.json(`/mlm/(${input1}, ${input2}, ${input3}, ${input4})`).then(() => {
-  // var output = document.getElementsByClassName("body");
   d3.json(`/mlm/(${input1}, ${input2}, ${input3}, ${input4})`).then(data => {
-    //console.log(data.data);
     
     result = data.data;
 
@@ -62,10 +48,12 @@ function makePredictions() {
         break;
     }
 
-
-      //   });
-
-    //   // Use the first sample from the list to build the initial plots
-    //   const firstMethod = Method[1];
   });
 }
+
+
+function init(){
+    document.getElementById("result-img").src="../static/css/theVillain.jpg";
+    document.getElementById("result-text").innerText="What will our AI predict???";
+};
+init();
