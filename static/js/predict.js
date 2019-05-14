dbd2d2d237c91ab1062f7b57f70f15203cc9a273
alyzaw <alyzaw@gmail.com>
2019-05-14 19:47:43 -0400

    current

--- static/js/predict.js
+++ static/js/predict.js
@@ -1,21 +1,3 @@
-// function input1Change(val){
-//     input1= val
-//   }
-//   function input2Change(val){
-//     input2= val
-//   }
-//   function input3Change(val){
-//     input3= val
-//   }
-//   function input4Change(val){
-//     input4= val
-//   }
-// function makePredictions() {
-
-//     var guess = d3.json(`/mlm/(${input1}, ${input2}, ${input3}, ${input4})`)
-//     var output = guess.getElementsByClassName("body")
-//     console.log(output)
-//   }
 
 function makePredictions() {
   // Grab a reference to the dropdown select element
@@ -30,30 +12,47 @@ function makePredictions() {
     
     result = data.data;
 
+    
+
     switch(result)
     {
-        case '0':
-        document.getElementById("result-img").src="https://previews.123rf.com/images/vadimdesign/vadimdesign1503/vadimdesign150300021/38032099-contraception-method-surgical-sterilization-icon-family-and-parenthood-pregnancy.jpg";
-        document.getElementById("result-text").innerText="Snip it";
-        break;
+      case '0':
+      document.getElementById("result-img").src="https://previews.123rf.com/images/vadimdesign/vadimdesign1503/vadimdesign150300021/38032099-contraception-method-surgical-sterilization-icon-family-and-parenthood-pregnancy.jpg";
+      document.getElementById("result-text").innerText="Surgical Sterilization";
+      break;
 
-        case '1':
-        document.getElementById("result-img").src="../static/css/pill_glyph.PNG";
-        document.getElementById("result-text").innerText="pill";
-        break;
+      case '1':
+      document.getElementById("result-img").src="../static/css/pill_glyph.PNG";
+      document.getElementById("result-text").innerText="IUD/Implant";
+      break;
 
-        case '2':
-        document.getElementById("result-img").src="../static/css/iud_glyph.PNG";
-        document.getElementById("result-text").innerText="IUD";
-        break;
+      case '2':
+      document.getElementById("result-img").src="../static/css/iud_glyph.PNG";
+      document.getElementById("result-text").innerText="Pill/Ring/Patch/Shot";
+      break;
+
+      case '3':
+      document.getElementById("result-img").src="../static/css/iud_glyph.PNG";
+      document.getElementById("result-text").innerText="Condoms";
+      break;
+
+      case '5':
+      document.getElementById("result-img").src="../static/css/iud_glyph.PNG";
+      document.getElementById("result-text").innerText="Withdrawal/Calender";
+      break;
+
+      default:
+      document.getElementById("result-img").src="../static/css/david.jpg";
+      document.getElementById("result-text").innerText="Shoot, I dunno";
+      break;
     }
 
   });
 }
 
 
-function init(){
-    document.getElementById("result-img").src="../static/css/theVillain.jpg";
-    document.getElementById("result-text").innerText="What will our AI predict???";
-};
-init();
+// function init(){
+//     document.getElementById("result-img").src="../static/css/theVillain.jpg";
+//     document.getElementById("result-text").innerText="What will our AI predict???";
+// };
+// init();
