5a1c8b403d6709e858a6e454b44f74149f1defc1
Deborah'syemojarepo <deborah.aina@outlook.com>
2019-05-14 21:57:33 -0400

    Updating Data Table

--- static/js/table.js
+++ static/js/table.js
@@ -53,6 +53,26 @@
         panes[i].style = "display: none";
     }
 
+    var panes = document.getElementsByClassName("pane");
+    var searchText = "Source";
+    
+    for (var i = 0; i < panes.length; i++)
+    {
+      if(panes[i].innerHTML.indexOf(searchText) !== -1) 
+        panes[i].style = "display: none";
+    }
+
+
+    var panes = document.getElementsByClassName("pane");
+    var searchText = "Brand";
+    
+    for (var i = 0; i < panes.length; i++)
+    {
+      if(panes[i].innerHTML.indexOf(searchText) !== -1) 
+        panes[i].style = "display: none";
+    }
+
+
 
   });
 // });
